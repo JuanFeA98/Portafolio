2c8cb5888ce27c6c7311fb32c61ab9d157900ed1
JuanFe <jmartinezbernal02@gmail.com>
2023-01-01 22:20:58 -0500

    Reset

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import Proyectos from './pages/Proyectos'
-import NotFound from './pages/NotFound'
-
-class App extends React.Component{
-    render(){
-        return(
-            <BrowserRouter>
-                <Layout>
-                    <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/proyectos" component={Proyectos}/>
-                        <Route component={NotFound}/>
-                    </Switch>
-                </Layout>
-            </BrowserRouter>
-        )
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/utils/Menu.js
+++
@@ -1,7 +0,0 @@
-const showMenu = ()=>{
-    const navBar = document.getElementById('NavBar')
-    navBar.classList.toggle('show-menu')
-}
-
-export default showMenu;
-
